da02435c2597026b4201fe664b22c37f99ec20b5
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-02-15 19:12:35 +0100

    change notification retry

--- rbz_api/wsgi.py
+++ rbz_api/wsgi.py
@@ -1,4 +1,10 @@
 from rbz_api.app import app
+import logging.config
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
+    log = logging.getLogger(__name__)
+    log.error(
+        "345Teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest")
+
     app.run(host='0.0.0.0', debug=True)
